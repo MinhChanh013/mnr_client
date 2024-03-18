767eceea29b9f652506180405b705a18b940f96d
Vũ Đức Nam <vdnam1207@gmail.com>
2024-03-18 10:48:21 +0000

    Namvd@2024 03 18

---
+++ src/views/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+function Dashboard() {
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   const token = localStorage.getItem("token");
+  //   if (!token) {
+  //     navigate("/login");
+  //   }
+  // });
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Dashboard;
