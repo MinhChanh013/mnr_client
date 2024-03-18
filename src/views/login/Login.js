e067df514fecfc31c5fce37224ae9fa87193a2b5
Vũ Đức Nam <vdnam1207@gmail.com>
2024-03-18 08:06:09 +0000

    Namvd@2024 03 18

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,10 +1,13 @@
-import classNames from "classnames/bind";
-import styles from "./Login.module.css";
+import { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
-import thumb from "../../assets/images/Thumb.png";
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./Login.module.scss";
+import { loginApi } from "../../apis/Login";
+
 import logo1 from "../../assets/images/logo.png";
 import logo2 from "../../assets/images/logo2.png";
-import Background from "../../assets/images/Background.png";
 import {
   UserOutlined,
   LockOutlined,
@@ -21,170 +24,191 @@ import {
   Typography,
   Form,
   Checkbox,
+  Card,
+  message,
 } from "antd";
 
-import { Link } from "react-router-dom";
 const cx = classNames.bind(styles);
-
 const { Title } = Typography;
 
 function Login() {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [messageApi, contextHolder] = message.useMessage();
+  const success = () => {
+    messageApi.open({
+      type: "success",
+      content: "Đăng nhập thành công",
+    });
+  };
+
+  const error = (message) => {
+    messageApi.open({
+      type: "error",
+      content: message,
+    });
+  };
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("/");
+    }
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!Email || !Password) {
+    } else {
+      let res = await loginApi({
+        username: Email,
+        password: Password,
+      });
+      if (res && res.access_token) {
+        localStorage.setItem("token", res.access_token);
+        success();
+        navigate("/");
+      } else if (res && res.status === 400) {
+        error(res.message);
+      }
+    }
+  };
   return (
-    <Flex className={cx("Wrapper")} align="center" justify="center">
-      <img src={Background} className={cx("Background")}></img>
-
-      <Flex className={cx("Content")} align="center" justify="center">
-        <Flex className={cx("Backgroundlogin")} align="center" justify="center">
-          <Row
-            gutter={[140, 8]}
-            style={{ width: "100%", height: "100%" }}
-            justify={"center"}
-            align={"middle"}
-          >
-            <Col
-              span={12}
-              style={{
-                paddingRight: "0",
-                paddingLeft: "60px",
-              }}
-            >
-              <Row
-                justify={"center"}
-                align={"center"}
-                style={{ width: "100%" }}
-              >
-                <img src={logo2} className={cx("logo2")} alt="logo"></img>
-              </Row>
-              <Row
-                justify={"center"}
-                align={"center"}
-                style={{ width: "100%" }}
-              >
-                <img src={thumb} className={cx("Thumb")} alt="thumb"></img>
-              </Row>
-              <Row
-                justify={"center"}
-                align={"center"}
-                style={{ width: "100%" }}
-              >
-                <Col
-                  span={24}
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  <img src={logo1} className={cx("logo")} alt="thumb"></img>
-                  <Title level={5} style={{ margin: "0" }}>
-                    copyright by Ceh software version 1.2 2023
-                  </Title>
-                </Col>
-              </Row>
-            </Col>
-            <Col
-              span={12}
-              style={{
-                padding: "0",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%",
-              }}
-            >
-              <Flex className={cx("Loginform")}>
-                {/*-------------------------logo------------------------------*/}
-                {/* <Typography className={cx('Title')}>C.A.S</Typography> */}
-                <img src="./logo.png" className={cx("Mainlogo")}></img>
-                <Typography className={cx("Title")}>
-                  Custom Automatic Systems
-                </Typography>
-                <Form
-                  name="basic"
-                  style={{
-                    width: "80%",
-                  }}
-                  initialValues={{
-                    remember: true,
-                  }}
-                  autoComplete="off"
-                >
-                  <Form.Item
-                    name="username"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your username!",
-                      },
-                    ]}
-                  >
-                    <Input
-                      className={cx("ipInfor")}
-                      placeholder="Tên người dùng"
-                      prefix={<UserOutlined className={cx("Icon")} />}
-                    />
-                  </Form.Item>
-
-                  <Form.Item
-                    name="password"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your password!",
-                      },
-                    ]}
-                  >
-                    <Input.Password
-                      className={cx("ipInfor")}
-                      placeholder="Mật khẩu"
-                      prefix={<LockOutlined className={cx("Icon")} />}
-                      iconRender={(visible) =>
-                        visible ? <EyeInvisibleOutlined /> : <EyeOutlined />
-                      }
-                    />
-                  </Form.Item>
-
-                  <Form.Item name="remember" valuePropName="checked">
-                    <Flex align="center" justify="space-between">
-                      <Checkbox
-                        style={{
-                          fontSize: "1rem",
-                          color: "#3b4bc2",
-                          fontWeight: "bold",
-                        }}
-                      >
-                        Lưu đăng nhập
-                      </Checkbox>
-                      <Link
-                        style={{
-                          color: "#3b4bc2",
-                          fontSize: "1rem",
-                          fontWeight: "bold",
-                        }}
-                      >
-                        Quên mật khẩu?
-                      </Link>
+    <>
+      {contextHolder}
+      <Flex className={cx("Wrapper")} align="center" justify="center">
+        <Flex className={cx("Content")} align="center" justify="center">
+          <Card className={cx("Loginform")}>
+            <Row gutter={[4, 40]} justify={"center"} align={"middle"}>
+              <img src={logo2} className={cx("logo2")} alt="logo"></img>
+              <Col>
+                <Title level={5} style={{ color: "#2D328D", margin: "0px" }}>
+                  Công ty cổ phần càng Sài Gòn
+                </Title>
+                <Title level={5} style={{ color: "#2D328D", margin: "0px" }}>
+                  Chi nhánh Tân Thuận
+                </Title>
+              </Col>
+              <Col span={24} className={cx("Col")}>
+                <Row>
+                  <Col span={24} className={cx("Col")}>
+                    <Flex vertical align="center">
+                      <img
+                        src="./logo.png"
+                        className={cx("Mainlogo")}
+                        alt="logo"
+                      ></img>
+                      <Typography className={cx("Title")}>
+                        Custom Automatic Systems
+                      </Typography>
                     </Flex>
-                  </Form.Item>
+                  </Col>
+                  <Col span={24} className={cx("Col")}>
+                    <Flex style={{ width: "80%" }}>
+                      <Form
+                        style={{ width: "100%" }}
+                        wrapperCol={{ span: 24 }}
+                        name="basic"
+                        initialValues={{
+                          remember: true,
+                        }}
+                        autoComplete="off"
+                      >
+                        <Form.Item
+                          name="username"
+                          rules={[
+                            {
+                              required: true,
+                              message: "Please input your username!",
+                            },
+                          ]}
+                        >
+                          <Input
+                            className={cx("ipInfor")}
+                            placeholder="Tên người dùng"
+                            value={Email}
+                            prefix={<UserOutlined className={cx("Icon")} />}
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                        </Form.Item>
 
-                  <Form.Item>
-                    <Button
-                      className={cx("btnLogin")}
-                      type="default"
-                      htmlType="submit"
-                      icon={<LoginOutlined></LoginOutlined>}
-                    >
-                      Đăng nhập
-                    </Button>
-                  </Form.Item>
-                </Form>
-                {/*-----------------------------------------------------------*/}
-              </Flex>
-            </Col>
-          </Row>
+                        <Form.Item
+                          name="password"
+                          rules={[
+                            {
+                              required: true,
+                              message: "Please input your password!",
+                            },
+                          ]}
+                        >
+                          <Input.Password
+                            className={cx("ipInfor")}
+                            placeholder="Mật khẩu"
+                            prefix={<LockOutlined className={cx("Icon")} />}
+                            iconRender={(visible) =>
+                              visible ? (
+                                <EyeInvisibleOutlined />
+                              ) : (
+                                <EyeOutlined />
+                              )
+                            }
+                            value={Password}
+                            onChange={(e) => setPassword(e.target.value)}
+                          />
+                        </Form.Item>
+
+                        <Form.Item name="remember" valuePropName="checked">
+                          <Flex align="center" justify="space-between">
+                            <Checkbox
+                              style={{
+                                fontSize: "1rem",
+                                color: "#3b4bc2",
+                                fontWeight: "bold",
+                              }}
+                            >
+                              Lưu đăng nhập
+                            </Checkbox>
+                            <Link
+                              style={{
+                                color: "#3b4bc2",
+                                fontSize: "1rem",
+                                fontWeight: "bold",
+                              }}
+                            >
+                              Quên mật khẩu?
+                            </Link>
+                          </Flex>
+                        </Form.Item>
+
+                        <Form.Item>
+                          <Button
+                            className={cx("btnLogin")}
+                            type="default"
+                            htmlType="submit"
+                            icon={<LoginOutlined />}
+                            onClick={handleSubmit}
+                          >
+                            Đăng nhập
+                          </Button>
+                        </Form.Item>
+                      </Form>
+                    </Flex>
+                  </Col>
+                </Row>
+              </Col>
+              <Col span={24} className={cx("Col")}>
+                <img src={logo1} className={cx("logo")} alt="thumb"></img>
+                <Title
+                  level={5}
+                  style={{ margin: "0", paddingLeft: "10px", color: "#2C86FF" }}
+                >
+                  copyright by Ceh software version 1.2 2023
+                </Title>
+              </Col>
+            </Row>
+          </Card>
         </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 }
 
