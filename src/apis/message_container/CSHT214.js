68ee54b682c50c8a2cc5f00bac9d01ae84beab06
Thai <thaibinh9a2@gmail.com>
2024-03-26 11:07:59 +0700

    commit csht 214

--- src/apis/message_container/CSHT214.js
+++ src/apis/message_container/CSHT214.js
@@ -7,12 +7,6 @@ const cpath = (action) => {
 };
 
 ///---validate
-const validateLoad = (GroupID) => {
-  throw new Error();
-};
-const validateSend = () => {
-  throw new Error();
-};
 
 ///--process
 export const load = async (params) => {
@@ -33,11 +27,8 @@ export const load = async (params) => {
 };
 
 export const send = async (rows = []) => {
-  validateSend();
 
-  const formData = {
-    datas: rows,
-  };
+  const formData = rows;
 
   const data = await poster(cpath("send"), formData);
   return data;
