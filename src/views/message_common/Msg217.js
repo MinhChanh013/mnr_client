4cb2778a36f8b231d8376bb0662f792e0060ea23
Thai <thaibinh9a2@gmail.com>
2024-03-26 18:26:48 +0700

    message: 217, 227, 252, 367.8 and 367 caontainer

--- src/views/message_common/Msg217.js
+++ src/views/message_common/Msg217.js
@@ -317,7 +317,7 @@ const Msg217 = () => {
         </Col>
         <Col span={18}>
           <Card
-            style={{ borderRadius: "0px", height: 'calc(100vh - 111px)' }}
+            style={{ borderRadius: "0px", height: "calc(100vh - 111px)" }}
             className="main-card"
           >
             <Row>
