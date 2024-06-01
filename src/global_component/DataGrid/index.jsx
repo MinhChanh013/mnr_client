3b975f2e0dbd0015427dee3886871590bab1419f
Chanh Nguyen Minh <chanh.nguyenminh@smartit.co.jp>
2024-06-01 13:23:16 +0700

    demo

--- src/global_component/DataGrid/index.jsx
+++ src/global_component/DataGrid/index.jsx
@@ -413,7 +413,7 @@ const DataGrid = forwardRef(
           }}
           defaultColumnOptions={{
             sortable: true,
-            // resizable: true,
+            resizable: true,
             // minWidth: 100,
           }}
           sortColumns={sortColumns}
