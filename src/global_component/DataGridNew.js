0191132eeace9c3b3fa708bab7fa04cc0e356e1a
Chanh Nguyen Minh <chanh.nguyenminh@smartit.co.jp>
2024-06-01 12:42:17 +0700

    demo

--- src/global_component/DataGridNew.js
+++ src/global_component/DataGridNew.js
@@ -1,8 +1,97 @@
-import React from "react";
+import Title from "antd/es/skeleton/Title";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import ReactDataGrid from "react-data-grid";
 
-const DataGridNew = ({ rows = [], columns = [] }) => {
-  return <ReactDataGrid rows={rows} columns={columns} />;
+const DataGridNew = ({
+  rows = [],
+  columns = [],
+  className,
+  maxHeight = 720,
+  style
+}) => {
+  const reactDataGridRef = useRef();
+  const [sortColumns, setSortColumns] = useState([]);
+  const [currentRows, setCurrenRows] = useState([]);
+  const [currentPage, setCurrenPage] = useState(1);
+
+
+  useEffect(() => {
+    setCurrenRows([]);
+    setCurrenPage(1);
+  }, [rows]);
+
+
+  const getComparator = (sortColumn) => {
+    switch (sortColumn) {
+      ////// so sánh các dữ liệu kiểu số
+      case "ImExType":
+      case "SumCargoWeight":
+      case "ID":
+      case "IDRef":
+      case "STT":
+        return (a, b) => {
+          return a[sortColumn] - b[sortColumn];
+        };
+      ////// so sánh các dữ liệu kiểu chuỗi
+      default:
+        return (a, b) => {
+          if (
+            (a[sortColumn] === null && b[sortColumn] === null) ||
+            (a[sortColumn] === undefined && b[sortColumn] === undefined)
+          )
+            return 0;
+          else if (a[sortColumn] === null || a[sortColumn] === undefined)
+            return 1;
+          else if (b[sortColumn] === null || b[sortColumn] === undefined)
+            return -1;
+          else return a[sortColumn].localeCompare(b[sortColumn]);
+        };
+    }
+  };
+  
+  
+  const sortedRows = useMemo(() => {
+    if (sortColumns.length === 0) return currentRows;
+
+    return [...currentRows].sort((a, b) => {
+      for (const sort of sortColumns) {
+        const comparator = getComparator(sort.columnKey);
+        const compResult = comparator(a, b);
+        if (compResult !== 0) {
+          return sort.direction === "ASC" ? compResult : -compResult;
+        }
+      }
+      return 0;
+    });
+  }, [currentRows, sortColumns]);
+
+
+  return (
+    <ReactDataGrid
+      renderers={{
+        noRowsFallback: (
+          <Title
+            level={5}
+            style={{ color: "#818181", gridColumn: "1/-1", margin: "10px" }}
+          >
+            --- Không có dữ liệu ---
+          </Title>
+        ),
+      }}
+      ref={reactDataGridRef}
+      className={`rdg-light ${className}`}
+      style={{
+        height: "calc(100% - var(--height-toolbar) - 40px)",
+        maxHeight: maxHeight,
+        ...style,
+      }}
+      defaultColumnOptions={{ sortable: true, resizable: true }}
+      sortColumns={sortColumns}
+      onSortColumnsChange={setSortColumns}
+      rows={sortedRows}
+      columns={columns}
+    />
+  );
 };
 
 export default DataGridNew;
